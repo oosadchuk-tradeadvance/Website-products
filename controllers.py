30a38f96665000a63a2ff48c8fdb504bc6ef21fc
oosadchuk-tradeadvance <oosadchuk@tradeadvance.com.ua>
2016-03-28 16:15:41 +0300

    Add a table view to products on site

--- controllers.py
+++ controllers.py
@@ -1,10 +1,11 @@
 #-*- coding: utf-8 -*-
 from openerp import http
 
+
 class Prod(http.Controller):
     @http.route('/product/product/', auth='public', website=True)
-    def index(self, **kw):
+    def produ_index(self, **kw):
         Products = http.request.env['product.template']
-        return http.request.render('website_testing.index', {
+        return http.request.render('website_products.produ_index', {
             'products': Products.search([])
         })
--- models.py
+++ models.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-
-class Teachers(models.Model):
-    _name = 'academy.teachers'
-
-    name = fields.Char()
