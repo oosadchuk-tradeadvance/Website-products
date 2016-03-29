a4b1dde6e1f4d4f15f63c5572d051e525a911e26
oosadchuk-tradeadvance <oosadchuk@tradeadvance.com.ua>
2016-03-29 22:21:01 +0300

    new fixes

--- controllers.py
+++ controllers.py
@@ -7,7 +7,9 @@ class Prod(http.Controller):
     def produ_index(self, **kw):
         Products = http.request.env['product.template']
         Categories = http.request.env['product.category']
+        Orders = http.request.env['sale.order']
         return http.request.render('website_products.produ_index', {
             'products': Products.search([]),
             'categories': Categories.search([("parent_id", "!=", False)]),
+            'order': Orders.search([]),
         })
--- static/src/js/website_products.js
+++ static/src/js/website_products.js
@@ -1,14 +1,30 @@
-var $template = $(".template");
+// var $template = $(".template");
 
-var hash = 2;
+// var hash = 2;
 
-$(".btn-add-panel").on("click", function () {
+// $(".btn-add-panel").on("click", function () {
 
-    var $newPanel = $template.clone();
-    $newPanel.find(".collapse").removeClass("in");
-    $newPanel.find(".accordion-toggle").attr("href",  "#" + (++hash))
-             .text("Dynamic panel #" + hash);
-    $newPanel.find(".panel-collapse").attr("id", hash).addClass("collapse").removeClass("in");
-    $("#accordion1").append($newPanel.fadeIn());
+//     var $newPanel = $template.clone();
+//     $newPanel.find(".collapse").removeClass("in");
+//     $newPanel.find(".accordion-toggle").attr("href",  "#" + (++hash))
+//              .text("Dynamic panel #" + hash);
+//     $newPanel.find(".panel-collapse").attr("id", hash).addClass("collapse").removeClass("in");
+//     $("#accordion1").append($newPanel.fadeIn());
+// });
+
+$(document).ready(function(){
+    $(".btn-danger").click(function() {
+        var self = this;
+        var session = new openerp.Session();
+        var Orders = new openerp.Model(session, "sale.order");
+        // window.alert("0");
+        window.alert(self);
+        window.alert(Orders);
+        window.alert(session);
+        var ord = search([]);
+        var id_s = Orders.call('action_button_confirm', [ord]).done(function (results) {
+            window.alert("16");
+        });
+    window.alert("32");
+    });
 });
-
